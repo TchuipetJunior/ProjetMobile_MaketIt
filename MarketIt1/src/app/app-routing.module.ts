327fe945b269d67c5a54f7194f0928f369aea469
brondonnono <39686553+brondonnono@users.noreply.github.com>
2020-01-05 15:10:44 +0100

    Add files via upload
    apres ajout de la page liste produits

--- MarketIt1/src/app/app-routing.module.ts
+++ MarketIt1/src/app/app-routing.module.ts
@@ -5,11 +5,19 @@ const routes: Routes = [
   { path: '', redirectTo: 'signin', pathMatch: 'full' },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
   },
   {
     path: 'registration',
     loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  },
+  {
+    path: 'tab1',
+    loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
   },
   {
     path: 'signin',
